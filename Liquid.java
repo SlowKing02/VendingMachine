38ac80e0d109598273de26efe2221be0413d2355
bbonham <32850914+bbonham@users.noreply.github.com>
2018-12-17 17:52:48 -0500

    Initial

---
+++ Liquid.java
@@ -0,0 +1,18 @@
+public class Liquid
+{
+    public int Qnty[] = new int[5];
+    {
+        Qnty[0] = 10;
+        Qnty[1] = 10;
+        Qnty[2] = 10;
+    } 
+    public String[][] Drinks = new String[5][2];
+    {
+        Drinks[0][0] = ("Coke    ");
+        Drinks[0][1] = ("1.00$   "); 
+        Drinks[1][0] = ("Pepsi   ");
+        Drinks[1][1] = ("1.00$   ");         
+        Drinks[2][0] = ("Water   ");
+        Drinks[2][1] = ("1.00$   "); 
+    }
+}
